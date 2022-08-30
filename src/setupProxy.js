963e34dadc54b0ddab585e27aad46633b2f6bb88
user <user@this.com>
2022-08-30 16:30:01 -0400

    Updating IP

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
     app.use(proxy('/api', { target: 'http://localhost:8080/' }));
     app.use(proxy('/account', { target: 'http://localhost:8081/' })); 
 */	
-	app.use(proxy('/api', { target: 'http://10.108.80.4:8080/' }));
-	app.use(proxy('/account', { target: 'http://10.107.237.31:8081/' }));
+	app.use(proxy('/api', { target: 'http://10.103.225.54:8080/' }));
+	app.use(proxy('/account', { target: 'http://10.110.23.172:8081/' }));
 
 /*     app.use(proxy('/api', { target: 'http://10.111.48.90:8080/' }));
     app.use(proxy('/account', { target: 'http://10.104.111.92:8081/' })); */
